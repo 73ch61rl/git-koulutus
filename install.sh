3f34d9ac31e12d7d5efb99fbb18c14921fc307b1
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2016-02-17 13:32:39 +0200

    Update xcode cli tools script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -oe
 # Asennetaan homebrew, fish shell ja git koulutusta varten
 
 # Xcode CLI tools
-xcode-select --install || echo "xcode 👍"
+xcode-select --install > /dev/null 2>&1 || echo "xcode 👍"
 
 # Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || echo "homebrew 👍"
