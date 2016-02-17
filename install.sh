5eeff75fa38414d2ce9494a33bfb243c8bc86599
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2016-02-17 13:47:09 +0200

    Add echos before each step explaning what this step does

--- install.sh
+++ install.sh
@@ -5,17 +5,21 @@ set -oe
 # Asennetaan homebrew, fish shell ja git koulutusta varten
 
 # Xcode CLI tools
-xcode-select --install > /dev/null 2>&1 || echo "xcode 👍"
+echo "Asennettaan Xcode CLI Tools…"
+xcode-select --install > /dev/null 2>&1 || echo "Xcode 👍"
 
 # Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || echo "homebrew 👍"
+echo "Asennettaan Homebrew…"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || echo "Homebrew 👍"
 
 # Fish
+echo "Laitetaan fish sun defaultti shelliksi…"
 brew install fish
 sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
 chsh -s /usr/local/bin/fish
 
 # Asenna uusin git ja tig
+echo "Asennettaan uusin git ja tig…"
 brew install git tig
 
 # Maisc!
