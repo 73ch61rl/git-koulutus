58bc60bd743aeb7c5f02e82032f9bcb90c3dafde
Atte Kojo <atte.kojo@reaktor.com>
2016-02-17 14:10:44 +0200

    Silence installation commands
    At the same go, remove `xcode-select`, since homebrew already does that

--- install.sh
+++ install.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 
-set -oe
-
-# Asennetaan homebrew, fish shell ja git koulutusta varten
-
-# Xcode CLI tools
-echo "Asennettaan Xcode CLI Tools…"
-xcode-select --install > /dev/null 2>&1 || echo "Xcode 👍"
-
 # Homebrew
-echo "Asennettaan Homebrew…"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || echo "Homebrew 👍"
+echo "Asennetaan Homebrew…"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2>&1
 
 # Fish
 echo "Laitetaan fish sun defaultti shelliksi…"
-brew install fish
-sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
-chsh -s /usr/local/bin/fish
+brew install fish > /dev/null 2>&1
+if [ "x$SHELL" != x/usr/local/bin/fish ]; then
+    sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
+    chsh -s /usr/local/bin/fish
+fi
 
 # Asenna uusin git ja tig
-echo "Asennettaan uusin git ja tig…"
-brew install git tig
+echo "Asennetaan uusin git ja tig…"
+brew install git tig > /dev/null 2>&1
 
 # Maisc!
 echo "Asia kunnossa! 😎"
