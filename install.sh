4bb0181888c4e420306ef9d387c06b0a0f255509
Atte Kojo <atte.kojo@reaktor.com>
2016-02-25 08:46:06 +0200

    Check that fish exists before chsh
    Also add logging

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 # Homebrew
 echo "Asennetaan Homebrew…"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2>&1
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > install.log 2>&1
 
 # Fish
 echo "Laitetaan fish sun defaultti shelliksi…"
-brew install fish > /dev/null 2>&1
-if [ "x$SHELL" != x/usr/local/bin/fish ]; then
+brew install fish > install.log 2>&1
+if [ "x$SHELL" != x/usr/local/bin/fish -a -x /usr/local/bin/fish ]; then
     sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
     chsh -s /usr/local/bin/fish
 fi
 
 # Asenna uusin git ja tig
 echo "Asennetaan uusin git ja tig…"
-brew install git tig > /dev/null 2>&1
+brew install git tig > install.log 2>&1
 
 # Maisc!
 echo "Asia kunnossa! 😎"
